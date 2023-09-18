7181d14e0a8a20a7aa61d402c79767b1d9fbaac7
Chandler Bowman <doctorgrant99@gmail.com>
2023-09-18 16:21:52 -0600

    added ingredient form

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Eggs', 2),
+    new Ingredient('Pancetta', 6)
+  ]
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
