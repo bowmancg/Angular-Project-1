e0bf2b119b39c0efe02342e0a7fe0a326448df19
Chandler Bowman <doctorgrant99@gmail.com>
2023-09-18 14:07:02 -0600

    added model and a recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Carbonara', 'Hard to make.', 'https://imgs.search.brave.com/wdEQxXZwUbPS8mU4w2CJfrNrsTCzGGdl4pUtDisdKrg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTEz/Njg2NDY4My9waG90/by9zcGFnaGV0dGkt/Y2FyYm9uYXJhLmpw/Zz9zPTYxMng2MTIm/dz0wJms9MjAmYz1J/X3hLSnBCdTF5a054/czJiaVpTaW5Oc2Yt/TWc1MV9fYlNyZ253/aDdjdzZNPQ')
+  ]
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
